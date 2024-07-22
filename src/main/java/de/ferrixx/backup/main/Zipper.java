748d954fcb14e2953ad370a33c8dc58c47830d9d
Justin Ippen <ippen.justin@gmx.de>
2024-07-22 09:59:45 +0200

    Update Zipper.java

--- src/main/java/de/ferrixx/backup/main/Zipper.java
+++ src/main/java/de/ferrixx/backup/main/Zipper.java
@@ -1,8 +1,7 @@
 package de.ferrixx.backup.main;
 
-import de.ferrixx.backup.gui.guiCreator;
+import de.ferrixx.backup.gui.GuiCreator;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.*;
@@ -13,15 +12,17 @@ import java.util.zip.ZipOutputStream;
 public class Zipper {
 
     public static void zipFolder(String sourceFolder, String zipFolder) {
-        try(FileOutputStream fos = new FileOutputStream(zipFolder);
-            ZipOutputStream zos = new ZipOutputStream(fos)) {
+        try (FileOutputStream fos = new FileOutputStream(zipFolder);
+             ZipOutputStream zos = new ZipOutputStream(fos)) {
 
-            final Path sourcePath = Paths.get(sourceFolder, new String[0]);
+            final Path sourcePath = Paths.get(sourceFolder);
+
             Files.walkFileTree(sourcePath, new SimpleFileVisitor<Path>() {
-                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attr) throws IOException {
-                    guiCreator.log.setText("Ordner: " + sourcePath.relativize(dir).toString() + " \n" + guiCreator.log.getText());
+                @Override
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                    GuiCreator.log.setText("Ordner: " + sourcePath.relativize(dir).toString() + " \n" + GuiCreator.log.getText());
 
-                    if(!sourcePath.equals(dir)) {
+                    if (!sourcePath.equals(dir)) {
                         zos.putNextEntry(new ZipEntry(sourcePath.relativize(dir).toString() + "/"));
                         zos.closeEntry();
                     }
@@ -29,8 +30,9 @@ public class Zipper {
                     return FileVisitResult.CONTINUE;
                 }
 
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attr) throws IOException {
-                    guiCreator.log.setText("Datei: " + sourcePath.relativize(file).toString() + " \n" + guiCreator.log.getText());
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    GuiCreator.log.setText("Datei: " + sourcePath.relativize(file).toString() + " \n" + GuiCreator.log.getText());
                     zos.putNextEntry(new ZipEntry(sourcePath.relativize(file).toString()));
                     Files.copy(file, zos);
                     zos.closeEntry();
@@ -39,14 +41,9 @@ public class Zipper {
                 }
             });
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            guiCreator.log.setText("Es ist ein Fehler aufgetreten! \n" + guiCreator.log.getText());
         } catch (IOException e) {
             e.printStackTrace();
-            guiCreator.log.setText("Es ist ein Fehler aufgetreten! \n" + guiCreator.log.getText());
-        } ;
-
+            GuiCreator.log.setText("Es ist ein Fehler aufgetreten! \n" + GuiCreator.log.getText());
+        }
     }
-
 }
