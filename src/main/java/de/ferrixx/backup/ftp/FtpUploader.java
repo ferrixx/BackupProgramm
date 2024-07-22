6fcc462bc16da22e7ad940f3e2baa7b398f97425
Justin Ippen <ippen.justin@gmx.de>
2024-07-22 10:03:14 +0200

    Update FtpUploader.java

--- src/main/java/de/ferrixx/backup/ftp/FtpUploader.java
+++ src/main/java/de/ferrixx/backup/ftp/FtpUploader.java
@@ -1,90 +1,96 @@
 package de.ferrixx.backup.ftp;
 
-import de.ferrixx.backup.gui.guiCreator;
+import de.ferrixx.backup.gui.GuiCreator;
 import org.apache.commons.net.ftp.FTPClient;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class ftpUploader {
+public class FtpUploader {
 
-    public String server, username, password, fileName, fileNameOnServer;
-    public int port = 21;
+    private String server;
+    private String username;
+    private String password;
+    private String fileName;
+    private String fileNameOnServer;
+    private int port = 21;
+    private FTPClient ftpClient;
 
-    public ftpUploader(String server, String username, String password, String fileName, String fileNameOnServer) {
+    public FtpUploader(String server, String username, String password, String fileName, String fileNameOnServer) {
         this.server = server;
         this.username = username;
         this.password = password;
         this.fileName = fileName;
         this.fileNameOnServer = fileNameOnServer;
+        this.ftpClient = new FTPClient();
     }
 
-    public FTPClient ftpClient = new FTPClient();
-
     public void uploadFiles() {
-
         boolean error = false;
-        try{
+
+        try {
             File fileToUpload = new File(fileName);
-            try {
-                ftpClient.connect(server);
-            } catch(IOException e) {
-                guiCreator.log.setText("Es konnte keine Verbindung zum FTP Server hergestellt werden! \n Entweder ist die IP Falsch oder es ist kein FTP-Server vorhanden! \n" + guiCreator.log.getText());
-                error = true;
-            }
 
-            boolean done = false;
-            boolean login = false;
-
+            // Connect to FTP server
             try {
-                login = ftpClient.login(username, password);
+                ftpClient.connect(server, port);
             } catch (IOException e) {
-                e.printStackTrace();
+                GuiCreator.log.setText("Es konnte keine Verbindung zum FTP Server hergestellt werden! \n" +
+                        "Entweder ist die IP Falsch oder es ist kein FTP-Server vorhanden! \n" + GuiCreator.log.getText());
                 error = true;
             }
 
-            if(!login) {
-                guiCreator.log.setText("Deine angegebenen Logindaten sind nicht Korrekt! \n" + guiCreator.log.getText());
-                error = true;
-            } else {
-                ftpClient.enterLocalPassiveMode();
-            }
-
-            ftpClient.setFileType(2);
-
-            FileInputStream inputStream = new FileInputStream(fileToUpload);
-
-            if(error == false) {
-                guiCreator.log.setText("FTP Upload wurde gestartet \n" + guiCreator.log.getText());
-
+            // Login to FTP server
+            boolean login = false;
+            if (!error) {
                 try {
-                    done = ftpClient.storeFile(fileNameOnServer, inputStream);
-                } catch (IOException e){
-                    guiCreator.log.setText("Es ist ein Fehler aufgetreten! - Keine Internet Verbindung \n" + guiCreator.log.getText());
+                    login = ftpClient.login(username, password);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    error = true;
+                }
+
+                if (!login) {
+                    GuiCreator.log.setText("Deine angegebenen Logindaten sind nicht korrekt! \n" + GuiCreator.log.getText());
+                    error = true;
+                } else {
+                    ftpClient.enterLocalPassiveMode();
+                    ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE);
                 }
             }
 
-            if(done) {
-                File file = new File("C:/Users/" + System.getProperty("user.name") + "/Documents/" + fileNameOnServer);
-                file.delete();
-                guiCreator.log.setText("FTP Upload war erfolgreich! \n" + guiCreator.log.getText());
-            }
+            // Upload file
+            if (!error) {
+                GuiCreator.log.setText("FTP Upload wurde gestartet \n" + GuiCreator.log.getText());
 
-            inputStream.close();
+                try (FileInputStream inputStream = new FileInputStream(fileToUpload)) {
+                    boolean done = ftpClient.storeFile(fileNameOnServer, inputStream);
+                    if (done) {
+                        File file = new File("C:/Users/" + System.getProperty("user.name") + "/Documents/" + fileNameOnServer);
+                        file.delete();
+                        GuiCreator.log.setText("FTP Upload war erfolgreich! \n" + GuiCreator.log.getText());
+                    } else {
+                        GuiCreator.log.setText("Es ist ein Fehler beim FTP-Upload aufgetreten. \n" + GuiCreator.log.getText());
+                    }
+                } catch (IOException e) {
+                    GuiCreator.log.setText("Es ist ein Fehler aufgetreten! - Keine Internet Verbindung \n" + GuiCreator.log.getText());
+                }
+            }
         } catch (Exception e) {
-            guiCreator.log.setText("Es ist ein Fehler aufgetreten! \n" + guiCreator.log.getText());
+            GuiCreator.log.setText("Es ist ein Fehler aufgetreten! \n" + GuiCreator.log.getText());
             e.printStackTrace();
         } finally {
+            // Logout and disconnect from FTP server
             try {
-                if(ftpClient.isConnected()) ftpClient.logout(); ftpClient.disconnect();
+                if (ftpClient.isConnected()) {
+                    ftpClient.logout();
+                    ftpClient.disconnect();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
-                guiCreator.log.setText("Es ist ein Fehler aufgetreten! \n" + guiCreator.log.getText());
+                GuiCreator.log.setText("Es ist ein Fehler aufgetreten! \n" + GuiCreator.log.getText());
             }
         }
-
-
     }
-
 }
